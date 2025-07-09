5565055c64c7ea464d7348948dcf30fccf69d0d6
rahulraip <rahulraipurkar735@gmail.com>
2025-07-10 00:09:03 +0530

    added home route

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Welcome to Flask App"
+        })
 
 @app.route("/info")
 def info():
