c857fab02d98c5db92243fe1fb32fcb8604e9516
rahulraip <rahulraipurkar735@gmail.com>
2025-07-10 12:37:17 +0530

    app.py modified

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import datetime
+import random	
+from utils.logger import get_logs
 
 app = Flask(__name__)
 
@@ -20,6 +22,30 @@ def info():
 @app.route("/phone")
 def phone():
     return "1111222233"
+
+@app.route("/api/users")
+def get_users():
+    users = [
+            {"id":1,"name":"Rahul Raipurkar","role":"DevOps Engineer"},
+            {"id":2,"name":"Jack","role":"AI Engineer"}
+
+             ]
+    return jsonify(users)
+
+@app.route('/api/metrics')
+def get_metrics():
+    metrics = {
+        "cpu_usage": f"{random.randint(10, 90)}%",
+        "memory_usage": f"{random.randint(1024, 8192)} MB",
+        "active_containers": random.randint(1, 10),
+        "last_updated": str(datetime.datetime.utcnow())
+    }
+    return jsonify(metrics)
+
+@app.route('/api/logs')
+def get_system_logs():
+    logs = get_logs()
+    return jsonify({"logs": logs})
 
 
 if __name__ == '__main__':
---
+++ utils/logger.py
@@ -0,0 +1,7 @@
+def get_logs():
+	return [ 
+		"[INFO] Server started at 2025-07-10 12:00 UTC",
+        	"[INFO] Health check OK",
+        	"[WARNING] CPU usage > 80%",
+        	"[INFO] Deployment successful"
+]
